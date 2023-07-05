092a48c0c48104b1d498913fd832ca0ba6263f2c
vaishakhwalambe18 <vaishakh007@gmail.com>
2023-07-05 15:49:23 +0530

    First py ta fm

---
+++ Learnings/L1.py
@@ -0,0 +1,7 @@
+# L1
+print("Test")
+print(5)
+
+#l2 some fun programs - jarvis
+# l3 - pip & modules
+# l4
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # execution_driver()
-from Framework.lib_util import *
-
-str_end_point = 'https://reqres.in/api/users/?q=id=507'
-# response = requests.get(str_end_point)
-# print(response)
-# print(response.json())
-# print(response.status_code)
-resposne = kyAPI.get_API( str_end_point, '', '', '', '', '200')
-print(resposne.json())
+# from Framework.lib_util import *
+#
+# str_end_point = 'https://reqres.in/api/users/?q=id=507'
+# # response = requests.get(str_end_point)
+# # print(response)
+# # print(response.json())
+# # print(response.status_code)
+# resposne = kyAPI.get_API( str_end_point, '', '', '', '', '200')
+# print(resposne.json())
