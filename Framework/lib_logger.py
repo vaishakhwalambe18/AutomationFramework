8b0219d07585fcca6096178c55046097861a856d
vaishakhwalambe18 <vaishakh007@gmail.com>
2023-07-18 09:04:34 +0530

    First py ta fm

--- Framework/lib_logger.py
+++ Framework/lib_logger.py
@@ -8,12 +8,14 @@ class LogGen:
 
     @staticmethod
     def loggen():
+        '''Add commment to log file'''
         logging.basicConfig(filename="C:\AutomationFramework\Logsautomation.log",format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
         logger=logging.getLogger()
         logger.setLevel(logging.INFO)
         return logger
     @staticmethod
     def updateLog(typeofLog,actionPerformed,valuesConcatenated,resultsGenerated):
+        '''add strig to doc string'''
         f = open("C:\AutomationFramework\logs.txt", "a+")
         ct = datetime.datetime.now()
         logString = typeofLog.upper()+" "+actionPerformed+" "+str(valuesConcatenated)+" "+str(resultsGenerated)
